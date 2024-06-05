2967fafaad7b3003926b746f1468f33b28a2a5fe
Ludovit Scholtz <ludko2@gmail.com>
2024-06-05 23:42:53 +0200

    authorize bff

--- ShindgApi/Controllers/BFFController.cs
+++ ShindgApi/Controllers/BFFController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ShindgApi.Model.Comm;
 
 namespace ShindgApi.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("v1")]
     public class BFFController : ControllerBase
